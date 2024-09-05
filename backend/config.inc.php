968df4b83083c0254bd5355b6b7040e8565ecd22
hsholt <hsholt@clemson.edu>
2024-09-05 14:08:42 -0400

    fixed files

--- backend/config.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-   define('HOST', 'sql110.infinityfree.com'); 
-   define('USERNAME', 'if0_36782893'); 
-   define('PASSWORD', 'Y1K8rTccJF1Gt'); 
-   define('DB_NAME', 'if0_36782893_todo_list'); 
-?>
