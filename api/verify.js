bae847589424dc7dd391e6f6e9f86bc09d3828cc
hsholt <hsholt@clemson.edu>
2024-09-09 20:23:16 -0400

    fixed files

--- api/verify.js
+++ api/verify.js
@@ -4,20 +4,18 @@ import jwt from 'jsonwebtoken';
 dotenv.config(); 
 
 export function verify_token(req, res, next) {
-
-    const authHeader = req.header['authorization']; 
+    const authHeader = req.headers['authorization']; 
     const token = authHeader && authHeader.split(' ')[1]; 
     
-    
-    if(!token) {
-        return res.status(401).json({ message: 'Access token required!'}); 
+    if (!token) {
+        return res.status(401).json({ message: 'Access token required!' }); 
     }
 
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-        if(err) {
-            return res.status(403).json({ message: 'Invalid Token or Expired Token'}); 
+        if (err) {
+            return res.status(403).json({ message: 'Invalid or Expired Token' }); 
         }
-        req.user = user; 
-        next(); 
-    })
+        req.user = user; // Attach the decoded user information to the request
+        next(); // Proceed to the next middleware or route handler
+    });
 }
