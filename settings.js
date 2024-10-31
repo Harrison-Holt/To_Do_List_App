808d43f329be6bf3252531f3f6a927e6f84df32f
hsholt <hholt2901@gmail.com>
2024-10-31 10:53:23 -0400

    fixed the syntax error

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 
 const display_username = document.getElementById("username"); 
 
-const token = localStorage.getItem(token); 
+const token = localStorage.getItem('token'); 
 
 const payload = JSON.parse(atob(token.split('.')[1])); 
 
