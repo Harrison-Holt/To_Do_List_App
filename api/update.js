5694a496ba9eba695f57ae6f986c69e02d53e805
hsholt <hholt2901@gmail.com>
2024-10-31 12:10:36 -0400

    added update username api

---
+++ api/update.js
@@ -0,0 +1,36 @@
+import pool from '../db.js'; 
+
+export default async function handler(req, res) {
+    
+    if(req.method !== 'POST') {
+        res.status(405).json({ message: 'Only POST Method Allowed!'}); 
+        return; 
+    }
+
+    const {new_username, user_id } = req.body; 
+
+    if(!new_username || !user_id) {
+        res.status(400).json({ message: 'new username required!'}); 
+        return; 
+    }
+
+    try {
+
+        const result = await pool.query(
+            'UPDATE users SET username = $1 WHERE id = $2', 
+            [new_username, user_id]
+        ); 
+
+        if(result.rowCount === 0) {
+            res.status(404).json({ message: 'User not found in DB!'}); 
+            return; 
+        }
+
+        res.status(200).json({ message: 'Username has been updated!'}); 
+
+    } catch(error) {
+        console.error('Error occurred during request processing: ', error); 
+        res.status(500).json({ message: 'Internal Server Error!'}); 
+        return; 
+    }
+}
--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-
+// display user info section 
 const display_username = document.getElementById("username"); 
 const display_email = document.getElementById("email"); 
 
@@ -8,7 +8,34 @@ const payload = JSON.parse(atob(token.split('.')[1]));
 
 const username = payload.username; 
 const email = payload.email; 
+const user_id = payload.userId; 
 
 display_username.innerHTML = `${username}`; 
 display_email.innerHTML = `${email}`; 
 
+
+// update user info section 
+async function update_username(new_username, user_id) {
+    try {
+        const response = await fetch('/api/update', {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json'
+            }, 
+            body: JSON.stringify(new_username, user_id) 
+        }); 
+
+        const data = await response.json(); 
+        console.log(data); 
+
+    } catch(error) {
+        console.error("Error fetching data", error); 
+    }
+}
+document.getElementById('update_username_button').addEventListener('click', (event) => {
+    event.preventDefault(); 
+
+    const new_username = document.getElementById('username_input').value; 
+    
+    update_username(new_username, user_id); 
+}); 
