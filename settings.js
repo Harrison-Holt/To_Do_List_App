0b90fc92c7c9d7268e73aaca47855b669ce8c2de
hsholt <hholt2901@gmail.com>
2024-10-31 11:08:38 -0400

    change class to id in username div

--- settings.js
+++ settings.js
@@ -1,15 +1,14 @@
 
 const display_username = document.getElementById("username"); 
+const display_email = document.getElementById("email"); 
 
 const token = localStorage.getItem('token'); 
 
 const payload = JSON.parse(atob(token.split('.')[1])); 
 
-console.log(payload); 
-console.log(payload.username); 
-
 const username = payload.username; 
 const email = payload.email; 
 
 display_username.innerHTML = `${username}`; 
+display_email.innerHTML = `${email}`; 
 
